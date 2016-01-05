aa352ead0d97f43c804331067c88790bb1759fd8
Jack Bennett <me@jackben.net>
2016-01-05 23:14:53 +0000

    Load a chart of my laptimes karting.

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+var x = d3.scale.linear().range([0, width])
+var y = d3.scale.linear().range([height, 0])
+
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+    .orient("bottom")
+    .tickFormat(d3.format("d"))
+    .tickSubdivide(0);
+
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(5);
+
+window.onload = function(){
+
+// Adds the svg
+var svg = d3.select("#chart")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json('/data/time6.json',function(err, data){
+
+    // Scale the range of the data
+
+    x.domain([0,data.jack.length]);
+    y.domain(d3.extent(data.jack)).nice();
+
+    // Add the valueline path.
+    svg.selectAll('.dot')
+      .data(data.jack).enter().append("circle")
+      .attr('class', 'dot jack')
+      .attr('r', 5)
+      .attr('cx', function(d,i){ return x(i+1) })
+      .attr('cy', function(d,i){ return y(d) })
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+})
+
+}
